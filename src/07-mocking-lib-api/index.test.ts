bbe1103f3f671a006e80dceadd6a66fe26cdc191
i.bykhavets <i.bykhavets@softarex.com>
2023-07-02 23:05:05 +0200

    feat: complete 07-mocking-lip-api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,69 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const baseURL = 'https://jsonplaceholder.typicode.com';
+    const testUrl = 'https://test.page.com';
+    const data = [
+      { id: 0, name: 'User1' },
+      { id: 1, name: 'User2' },
+      { id: 2, name: 'User3' },
+    ];
+
+    (axios.create as jest.Mock).mockReturnValue({
+      get: jest.fn().mockResolvedValue({ data }),
+    });
+
+    await throttledGetDataFromApi(testUrl);
+
+    expect(axios.create).toHaveBeenCalledWith({ baseURL });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const testUrl = 'https://test.page.com';
+    const data = [
+      { id: 0, name: 'User1' },
+      { id: 1, name: 'User2' },
+      { id: 2, name: 'User3' },
+    ];
+
+    (axios.create as jest.Mock).mockReturnValue({
+      get: jest.fn().mockResolvedValue({ data }),
+    });
+
+    await throttledGetDataFromApi(testUrl);
+
+    jest.runAllTimers();
+
+    const instance = axios.create();
+    expect(instance.get).toHaveBeenCalledWith(testUrl);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const testUrl = 'https://test.page.com';
+    const data = [
+      { id: 0, name: 'User1' },
+      { id: 1, name: 'User2' },
+      { id: 2, name: 'User3' },
+    ];
+
+    (axios.create as jest.Mock).mockReturnValue({
+      get: jest.fn().mockResolvedValue({ data }),
+    });
+
+    const responseDate = await throttledGetDataFromApi(testUrl);
+
+    expect(responseDate).toBe(data);
   });
 });
