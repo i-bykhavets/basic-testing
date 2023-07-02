4a68967379117e07705e49e442b18c052801f8e6
i.bykhavets <i.bykhavets@softarex.com>
2023-07-03 00:58:17 +0200

    feat: complete 08-snapshot-testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,23 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const lastNode = { value: null, next: null };
+    const fourthNode = { value: 1, next: lastNode };
+    const thirdNode = { value: 1, next: fourthNode };
+    const secondNode = { value: 1, next: thirdNode };
+    const firstNode = { value: 1, next: secondNode };
+    const expectedListHead = firstNode;
+
+    const elementsForList = [1, 1, 1, 1];
+
+    expect(generateLinkedList(elementsForList)).toStrictEqual(expectedListHead);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const elementsForList = [2, 2, 2, 2, 2, 2, 2, 2];
+    expect(generateLinkedList(elementsForList)).toMatchSnapshot();
   });
 });
